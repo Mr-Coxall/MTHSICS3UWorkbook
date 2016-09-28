ed73ce971d1275b7db97e17b323142ded7d1d385
Patrick Coxall <mr.coxall@mths.ca>
2016-09-28 01:15:32 -0400

    test
    See if I can add text into top of playground

--- Contents/Chapters/Intro.playgroundchapter/Pages/Intro-01.playgroundpage/Contents.swift
+++ Contents/Chapters/Intro.playgroundchapter/Pages/Intro-01.playgroundpage/Contents.swift
@@ -1,3 +1,20 @@
+//#-hidden-code
+//
+//  Contents.swift
+//
+//  Copyright (c) 2016 Apple Inc. All Rights Reserved.
+//
+//#-end-hidden-code
+/*:
+ **Goal:** Use Swift commands to tell Byte to move and collect a gem.
+ 
+ Your character, Byte, loves to collect gems but can't do it alone. In this first puzzle, you'll need to write Swift [commands](glossary://command) to move Byte across the puzzle world to collect a gem.
+ 
+ 1. steps: Look for the gem in the puzzle world.
+ 2. Enter the correct combination of the `moveForward()` and `collectGem()` commands.
+ 3. Tap Run My Code.
+*/
+
 //#-code-completion(identifier, hide, setupLiveView())
 //#-hidden-code
 import Foundation
